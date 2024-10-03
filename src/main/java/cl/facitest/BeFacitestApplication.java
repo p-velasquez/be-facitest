d8218ce36779c94d4b74d381f1d61395387beb19
p-velasquez <pablovelasquezr@gmail.com>
2024-10-03 20:22:53 -0300

    first commit

---
+++ src/main/java/cl/facitest/BeFacitestApplication.java
@@ -0,0 +1,13 @@
+package cl.facitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFacitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeFacitestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/facitest/BeFacitestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.facitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFacitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
